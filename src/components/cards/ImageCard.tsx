d63355c58a3858da1bd30faea0d2db037e2a133a
ArtiGaund <artigaund2210@gmail.com>
2025-04-15 18:20:39 +0530

    Added coding block in second image card of hero section

--- src/components/cards/ImageCard.tsx
+++ src/components/cards/ImageCard.tsx
@@ -1,17 +1,24 @@
 export default function ImageCard({
+    width,
+    height,
     color,
     position,
     shadow,
+    children,
 }: {
+    width: number;
+    height: number;
     color: string;
     position: string;
     shadow: string;
+    children: any;
 }){
     return(
         <div
+        style={{ width: width, height: height }}
         className={`w-[450px] h-[600px] absolute ${color} rounded-3xl ${position} ${shadow}`}
         >
-
+            {children}
         </div>
     )
 }
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,9 +1,34 @@
 "use client";
 import ImageCard from "../cards/ImageCard";
 import Circle from "../sub-components/Circle";
+import { CodeBlock } from "../ui/code-block";
 import { SocialNetworkLink } from "../ui/social-network-link";
 
+
+
 export default function HeroSection() {
+  const code = `const DummyComponent = () => {
+    const [count, setCount] = React.useState(0);
+   
+    const handleClick = () => {
+      setCount(prev => prev + 1);
+    };
+   
+    return (
+      <div className="p-4 border rounded-lg">
+        <h2 className="text-xl font-bold mb-4">Fights Counter</h2>
+        <p className="mb-2">Fight Club Fights Count: {count}</p>
+        <button 
+          onClick={handleClick}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  };
+  `;
+
     return(
              <div className="relative w-full h-screen overflow-hidden">
       {/* Left circles */}
@@ -23,10 +48,14 @@ export default function HeroSection() {
       />
 
       <ImageCard 
+      width={450}
+      height={600}
       color=" bg-red-700"
       position="top-[-50px] left-[300px] z-20"
       shadow="shadow-[0px_0px_100px_40px_black]"
-      />
+      >
+        <div></div>
+        </ImageCard>
       {/* White line */}
       <div className="absolute top-[150px] left-1/2 w-[150px] h-[1px] bg-white rounded-full"></div>
       {/* Title */}
@@ -61,10 +90,19 @@ export default function HeroSection() {
   blur="blur-3xl"
 />
 <ImageCard 
+    width={600}
+    height={500}
       color="bg-yellow-400"
       position="bottom-[-50px] left-[200px] z-10"
       shadow="shadow-[0px_0px_100px_40px_black]"
-      />
+      >
+        <CodeBlock 
+        language="jsx"
+        filename="DummyComponent.jsx"
+        highlightLines={[9,13,14,18]}
+        code={code}
+        />
+      </ImageCard>
 {/* Bottom center circle */}
 <Circle
   size={200}
